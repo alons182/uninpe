751d950ce35a264845cab18fa60aa84c36dcdf31
alonso <alons182@hotmail.com>
2013-11-07 20:05:53 -0600

    initial site

---
+++ logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-10-25 17:13:09 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-10-25T17:13:09+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-10-25T17:13:16+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-11-07T16:34:20+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-11-07T20:50:12+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
