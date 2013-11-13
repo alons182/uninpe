6b53d077f1f0c3a630443546daa8afa623367308
alonso <alons182@hotmail.com>
2013-11-12 23:29:52 -0600

    cabio menu home programas

--- templates/uninpe/js/main.js
+++ templates/uninpe/js/main.js
@@ -1,3 +1,25 @@
 jQuery(function($) {
-	console.log('test');
+
+	$menu = $('#middle-content').find('.menu');
+           
+    $menu.find('.item-139').find('.item-131 a').append('<span class="subtitle">Énfasis en Dirección General</span>');
+    $menu.find('.item-139').find('.item-132 a').append('<span class="subtitle">Énfasis en Dirección Comercial</span>');
+    $menu.find('.item-139').find('.item-133 a').append('<span class="subtitle">Énfasis en Dirección de Recursos Humanos</span>');
+    $menu.find('.item-139').find('.item-134 a').append('<span class="subtitle">Énfasis en Dirección de Gestión de Calidad</span>');
+
+    $menu.find('.item-140').find('.item-135 a').append('<span class="subtitle">Énfasis en Dirección General</span>');
+    $menu.find('.item-140').find('.item-136 a').append('<span class="subtitle">Énfasis en Dirección Comercial</span>');
+    $menu.find('.item-140').find('.item-137 a').append('<span class="subtitle">Énfasis en Dirección de Recursos Humanos</span>');
+    $menu.find('.item-140').find('.item-138 a').append('<span class="subtitle">Énfasis en Dirección de Control y Gestión</span>');
+
+    $menu.find('.item-155').find('.item-156 a').append('<span class="subtitle">Énfasis en Dirección General</span>');
+    $menu.find('.item-155').find('.item-157 a').append('<span class="subtitle">Énfasis en Dirección Comercial</span>');
+    $menu.find('.item-155').find('.item-158 a').append('<span class="subtitle">Énfasis en Dirección de Recursos Humanos</span>');
+    $menu.find('.item-155').find('.item-159 a').append('<span class="subtitle">Énfasis en Dirección de Gestión de Calidad</span>');
+
+    $menu.find('.item-160').find('.item-161 a').append('<span class="subtitle">Énfasis en Dirección General</span>');
+    $menu.find('.item-160').find('.item-162 a').append('<span class="subtitle">Énfasis en Dirección Comercial</span>');
+    $menu.find('.item-160').find('.item-163 a').append('<span class="subtitle">Énfasis en Dirección de Recursos Humanos</span>');
+    $menu.find('.item-160').find('.item-164 a').append('<span class="subtitle">Énfasis en Dirección de Control y Gestión</span>');
+    
 });
